5a668041ed30a55476caab3e383ca7cfdda798c0
reckcp9-code <reck.cp9@gmail.com>
2026-09-08 01:08:26 -0300

    fix: renovar cache PWA após varredura geral

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const DF_CACHE='df-extrusor-shell-v13';
+const DF_CACHE='df-extrusor-shell-v14';
 const STATE_CACHE='df-extrusor-state-v1';
 const API='https://df-extrusor-api.reck-cp9.workers.dev';
 const HISTORICAL_APP='https://raw.githubusercontent.com/reckcp9-code/calculadora-extrusao/3e570fc08be61679377cd81eb4e90bc45216f4c2/app.html';
