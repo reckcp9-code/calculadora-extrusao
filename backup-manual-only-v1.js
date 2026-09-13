2247744890df5d90a098624a690669d6ff0366cb
reckcp9-code <reck.cp9@gmail.com>
2026-09-12 21:56:23 -0300

    Restaura backup manual como carregador leve

---
+++ backup-manual-only-v1.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+  var s=document.createElement('script');
+  s.src='./backup-manual-lite-v2.js?v=20260912-hotfix-trava-v148';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
