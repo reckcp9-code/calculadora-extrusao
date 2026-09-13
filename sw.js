4dd543345085bfe39e78ed68cdd2e2af0600e9a1
reckcp9-code <reck.cp9@gmail.com>
2026-09-12 21:57:02 -0300

    Renova cache da correção de estabilidade v148

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const DF_CACHE='df-extrusor-shell-v21';
+const DF_CACHE='df-extrusor-shell-v22';
 const STATE_CACHE='df-extrusor-state-v1';
 const API='https://df-extrusor-api.reck-cp9.workers.dev';
 const CORE=[
